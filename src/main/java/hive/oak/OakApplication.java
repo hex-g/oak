29588f8b85ed0df1a17b8e021b20a9a9ec3a1d03
Victor Hirumitsu <vmhirumitsu@gmail.com>
2019-05-14 18:53:16 -0300

    Initial commit

---
+++ src/main/java/hive/oak/OakApplication.java
@@ -0,0 +1,11 @@
+package hive.oak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(OakApplication.class, args);
+  }
+}
